a63ea561d1424fb1847eb41fa1ad88e7e89f9568
magesh <maheshvaran@gmail.com>
2019-02-23 12:33:34 +0530

    green star component initial checkin :)

---
+++ src/app/pages/performance/star/greenstar/greenstar.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-greenstar',
+  templateUrl: './greenstar.component.html',
+  styleUrls: ['./greenstar.component.scss'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class GreenstarComponent implements OnInit {
+  private holidayColorCode="#7beded";
+  private complaint100ColorCode = "#7CFC00";
+  private complaint75ColorCode = "#FFFF00";
+  private complaintBelow75ColorCode = "#FF0000";
+
+   
+ public day1FillColor  = this.complaint100ColorCode;
+  public day2FillColor  = this.complaint75ColorCode;
+  public day3FillColor  = this.complaintBelow75ColorCode;
+  public day4FillColor  = this.holidayColorCode;
+  public day5FillColor  = this.complaint100ColorCode;
+  public day6FillColor  = this.complaint75ColorCode;
+  public day7FillColor  = this.complaintBelow75ColorCode;
+  public day8FillColor  = this.holidayColorCode;
+  public day9FillColor  = this.complaint100ColorCode;
+  public day10FillColor = this.complaint75ColorCode;
+  public day11FillColor = this.complaintBelow75ColorCode;
+  public day12FillColor = this.holidayColorCode;
+  public day13FillColor = this.complaintBelow75ColorCode;
+  public day14FillColor = this.complaint75ColorCode;
+  public day15FillColor = this.holidayColorCode;
+  public day16FillColor = this.complaint100ColorCode;
+  public day17FillColor = this.complaint75ColorCode;
+  public day18FillColor = this.complaintBelow75ColorCode;
+  public day19FillColor = this.holidayColorCode;
+  public day20FillColor = this.complaint100ColorCode;
+  public day21FillColor = this.complaint75ColorCode;
+  public day22FillColor = this.complaintBelow75ColorCode;
+  public day23FillColor = this.holidayColorCode;
+  public day24FillColor = this.complaint100ColorCode;
+  public day25FillColor = this.complaint75ColorCode;
+  public day26FillColor = this.complaintBelow75ColorCode;
+  public day27FillColor = this.holidayColorCode;
+  public day28FillColor = this.complaint100ColorCode;
+  public day29FillColor = this.complaintBelow75ColorCode;
+  public day30FillColor = this.holidayColorCode;
+  public day31FillColor = this.complaint100ColorCode;
+  ngOnInit(){
+    
+    
+  }
+    constructor() {
+  
+    }
+  
+
+}
