637f3fd510d9f8babd120a0ce61f969d7c3c100e
magesh <maheshvaran@gmail.com>
2019-03-15 18:53:38 +0530

    Student Bulk Upload

--- src/app/pages/student/student-bulk-upload.component.modal.ts
+++ src/app/pages/student/student-bulk-upload.component.modal.ts
@@ -25,26 +25,26 @@ export class StudentBulkUploadModalComponent {
 
     private resetUploadParam() {
         this.uploadFile = null;
-       // this.fileName = '';
-      //  this.isDisableButton = true;
-       // this.isShowSuccessMsg = false;
-      //  this.isShowErrorMsg = false;
-      //  this.errorMessage = '';
-      //  this.errorMessages = [];
+        // this.fileName = '';
+        //  this.isDisableButton = true;
+        // this.isShowSuccessMsg = false;
+        //  this.isShowErrorMsg = false;
+        //  this.errorMessage = '';
+        //  this.errorMessages = [];
     }
 
-    public handleFileInput(files: FileList) {        
+    public handleFileInput(files: FileList) {
         this.resetUploadParam();
         console.log('I am handled');
-        if(files.item(0).name.split('.').pop() === 'xlsx') {
+        if (files.item(0).name.split('.').pop() === 'xlsx') {
             console.log('I am an excel file');
             this.uploadFile = files.item(0);
             this.fileName = files.item(0).name;
-          //  this.isDisableButton = false;
+            //  this.isDisableButton = false;
             //this.isShowSuccessMsg = false;
         } else {
-           // this.isShowErrorMsg = true;
-           // this.errorMessage = 'Please upload valid file in .xlsx file format';
+            // this.isShowErrorMsg = true;
+            // this.errorMessage = 'Please upload valid file in .xlsx file format';
         }
     }
 
@@ -55,7 +55,7 @@ export class StudentBulkUploadModalComponent {
         this.studentService.getStudentDataTemplate(searchParam).subscribe(
             (response) => {
                 var blob = new Blob([response], { type: 'application/octet-stream' });
-                tempSaveAs(blob, "sample_download.xlsx");
+                tempSaveAs(blob, "Bulk_Upload_Student_" + this.schoolId + "_" +new Date()+".xlsx");
             },
             error => {
                 console.log("Http Server error", error);
@@ -64,36 +64,36 @@ export class StudentBulkUploadModalComponent {
     }
 
     public uploadStudentBulkData(): void {
-console.log('I am into upload'+this.uploadFile);
-      //  this.isDisableButton = true;
-       // this.isShowSuccessMsg = false;
+        console.log('I am into upload' + this.uploadFile);
+        //  this.isDisableButton = true;
+        // this.isShowSuccessMsg = false;
 
         if (this.uploadFile != null) {
             console.log('I am not empty');
             const formData = new FormData();
             formData.append('file', this.uploadFile);
             formData.append('userId', 'Magesh');
-            formData.append('schoolId', this.schoolId+'');
+            formData.append('schoolId', this.schoolId + '');
 
-           // this.isSpinner = true;
+            // this.isSpinner = true;
             this.studentService.bulkUploadStudentData(formData).subscribe(
                 (response) => {
-                  //  this.isSpinner = false;
-                  console.log("response ==> " + response);
-                    if(response.message == null ) {
+                    //  this.isSpinner = false;
+                    console.log("response ==> " + response);
+                    if (response.message == null) {
                         console.log('response received success');
-                       // this.isShowErrorMsg = true;
+                        // this.isShowErrorMsg = true;
                         //this.errorMessage = '';
-                       // this.errorMessages = response.result;
-                       // this.isDisableButton = false;
+                        // this.errorMessages = response.result;
+                        // this.isDisableButton = false;
                     } else {
                         console.log('response received with error');
                         this.uploadFile = null;
                         this.fileName = '';
-                       // this.isDisableButton = true;
-                       // this.isShowSuccessMsg = true;
-                       // this.isShowErrorMsg = false;
-                      //  this.errorMessage = '';
+                        // this.isDisableButton = true;
+                        // this.isShowSuccessMsg = true;
+                        // this.isShowErrorMsg = false;
+                        //  this.errorMessage = '';
                     }
                 },
                 error => {
@@ -102,7 +102,7 @@ console.log('I am into upload'+this.uploadFile);
                 }
             );
         } else {
-           // this.isShowErrorMsg = true;
+            // this.isShowErrorMsg = true;
             //this.errorMessage = 'Please upload valid file in .xlsx file format';
         }
     }
